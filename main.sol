d2a17e5bb5b376f732fce2df5d796d4534d9d34e
lukasuti751 <lukasuti751@gmail.com>
2026-02-26 12:51:34 +0100

    Update main.sol

--- main.sol
+++ main.sol
@@ -74,3 +74,79 @@ contract Herbo is ReentrancyGuard, Pausable, Ownable {
     event RemedyTitleUpdated(uint256 indexed remedyId, bytes32 previousTitle, bytes32 newTitle, uint256 atBlock);
     event BatchRemediesLogged(uint256[] remedyIds, address indexed author, uint256 atBlock);
     event CampaignCreated(uint256 indexed campaignId, uint256 startBlock, uint256 endBlock, address indexed createdBy, uint256 atBlock);
+    event EntryJoinedCampaign(uint256 indexed entryId, uint256 indexed campaignId, uint256 atBlock);
+
+    // -------------------------------------------------------------------------
+    // ERRORS
+    // -------------------------------------------------------------------------
+
+    error HRB_ZeroAddress();
+    error HRB_ZeroEntryId();
+    error HRB_EntryNotFound();
+    error HRB_InvalidNameHash();
+    error HRB_InvalidBenefitHash();
+    error HRB_InvalidCategoryHash();
+    error HRB_NotCurator();
+    error HRB_NotWellnessKeeper();
+    error HRB_LedgerPaused();
+    error HRB_ReentrantCall();
+    error HRB_TransferFailed();
+    error HRB_ZeroAmount();
+    error HRB_InsufficientVitality();
+    error HRB_MaxEntriesReached();
+    error HRB_ArrayLengthMismatch();
+    error HRB_BatchTooLarge();
+    error HRB_ZeroBatchSize();
+    error HRB_InvalidVitalityRate();
+    error HRB_EntryAlreadyRemoved();
+    error HRB_NotEntryContributor();
+    error HRB_SameBenefitHash();
+    error HRB_MaxCategoriesReached();
+    error HRB_CategoryAlreadyExists();
+    error HRB_InvalidNoteHash();
+    error HRB_RemedyNotFound();
+    error HRB_InvalidRemedyRef();
+    error HRB_MaxRemediesReached();
+    error HRB_NotRemedyAuthor();
+    error HRB_RemedyAlreadyRemoved();
+    error HRB_InvalidTitleHashForRemedy();
+    error HRB_InvalidBlockRange();
+    error HRB_CampaignNotFound();
+    error HRB_CampaignNotActive();
+    error HRB_MaxCampaignsReached();
+    error HRB_EntryAlreadyInCampaign();
+    error HRB_InvalidCampaignRange();
+
+    // -------------------------------------------------------------------------
+    // CONSTANTS
+    // -------------------------------------------------------------------------
+
+    uint256 public constant HRB_BPS_BASE = 10000;
+    uint256 public constant HRB_MAX_FEE_BPS = 500;
+    uint256 public constant HRB_MAX_ENTRIES = 2500;
+    uint256 public constant HRB_MAX_CATEGORIES = 180;
+    uint256 public constant HRB_MAX_BATCH_LOG = 35;
+    uint256 public constant HRB_MAX_BATCH_CREDIT = 45;
+    uint256 public constant HRB_LEDGER_SALT = 0x8E4a2C6d0F3b5E7a9C1d4F6b8A0c2E5f7B9d1A4;
+    uint256 public constant HRB_VITALITY_SCALE = 1e18;
+
+    // -------------------------------------------------------------------------
+    // IMMUTABLES
+    // -------------------------------------------------------------------------
+
+    address public immutable curator;
+    address public immutable treasury;
+    address public immutable wellnessKeeper;
+    uint256 public immutable deployBlock;
+    bytes32 public immutable ledgerDomain;
+
+    // -------------------------------------------------------------------------
+    // STATE
+    // -------------------------------------------------------------------------
+
+    struct HerbEntry {
+        address contributor;
+        bytes32 nameHash;
+        bytes32 benefitHash;
+        bytes32 categoryHash;
+        uint256 loggedAtBlock;
